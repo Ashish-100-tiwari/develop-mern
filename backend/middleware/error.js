f01ea6e4fc0ab9c6910dbeadba57d996e61f1d9f
Ashish Tiwari <rkt987186@gmail.com>
2023-06-17 12:03:22 +0530

    Backend user & password authentication

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -6,8 +6,23 @@ module.exports=(err,req,res,next)=>{
     err.message=err.message||"Internal Server Error"
 // cast error wrong mongodb id error
 
-if(err.name==="CastError"){
-    const message= `Resourse not found. Invalid: ${err.path}`
+    if(err.name==="CastError"){
+        const message= `Resourse not found. Invalid: ${err.path}`
+        err= new ErrorHander(message,400);
+    }
+// mongoose duplicate key error
+    if(err.code===11000){
+        const message=`Duplicate ${Object.keys(err.keyValue)} Entered`;
+        err= new ErrorHander(message,400);
+    }
+// wrong json webtoken
+if(err.name==="JsonWebTokenError"){
+    const message= `Json web Token is invalid Try again`;
+    err= new ErrorHander(message,400);
+}
+// JWT expire error 
+if(err.name==="TokenExpiredError"){
+    const message= `Json web Token is Expired, Try again`;
     err= new ErrorHander(message,400);
 }
     res.status(err.statusCode).json({
