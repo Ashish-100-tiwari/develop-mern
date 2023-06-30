f6aca987860cd89d597b7357d5529a9a9199bb1c
Ashish Tiwari <rkt987186@gmail.com>
2023-06-30 23:44:33 +0530

    Added Product detail pages

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -18,6 +18,7 @@ exports.createProduct=catchAsyncErrors(async(req,res,next)=>{
 // Get All Product 
 exports.getAllProducts =catchAsyncErrors(async(req,res,next)=>{
     // return next(new ErrorHander("This is my temp error",500));
+    // return res.status(500).json({message:"hello kkfk"});
     const resultPerPage=8;
     const productCount=await Product.countDocuments();
     const apiFeature = new ApiFeatures(Product.find(),req.query).search().filter().pagination(resultPerPage);
--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,9 +1,9 @@
 import React, { Fragment, useEffect} from "react";
 import { CgMouse } from "react-icons/all";
 import "./Home.css";
-import Product from "./Product.js"
+import Product from "./ProductCard.js"
 import MetaData from "../layout/MetaData.js";
-import {getProduct}from "../../actions/productAction"
+import {clearErrors,getProduct}from "../../actions/productAction"
 import {useDispatch,useSelector}from "react-redux";
 import Loader from "../layout/Loader/Loader";
 import { useAlert } from "react-alert";
@@ -15,9 +15,9 @@ const Home = () => {
   const {loading,error,products}=useSelector(state=>state.products);
 
   useEffect(()=>{
-   
     if(error){
       return alert.error(error);
+      dispatch(clearErrors())
     }
     dispatch(getProduct());
   },[dispatch,error,alert]);
